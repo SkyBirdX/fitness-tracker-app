252d771e7eafa895254bd88831f212c7dc3a05dd
John <johnbroder5@gmail.com>
2025-06-30 20:28:57 +0200

    V6

--- src/components/MealForm.jsx
+++ src/components/MealForm.jsx
@@ -1,16 +1,46 @@
 import React, { useState } from "react";
-import MealForm from "../components/MealForm";
-import { initializeApp } from "firebase/app";
+import { db, auth } from "../firebase";
+import { collection, addDoc, query, where, orderBy, getDocs } from "firebase/firestore";
 
-function NutritionPage() {
-  const [meals, setMeals] = useState([]);
+function MealForm({ setMeals }) {
+  const [input, setInput] = useState({ name: "", protein: "", fat: "", carbs: "" });
+
+  const handleAdd = async (e) => {
+    e.preventDefault();
+    if (!auth.currentUser) {
+      alert("Du musst eingeloggt sein!");
+      return;
+    }
+
+    try {
+      await addDoc(collection(db, "meals"), {
+        uid: auth.currentUser.uid,
+        ...input,
+        protein: Number(input.protein),
+        fat: Number(input.fat),
+        carbs: Number(input.carbs),
+        date: new Date(),
+      });
+
+      setInput({ name: "", protein: "", fat: "", carbs: "" });
+
+      const q = query(
+        collection(db, "meals"),
+        where("uid", "==", auth.currentUser.uid),
+        orderBy("date", "desc")
+      );
+      const querySnapshot = await getDocs(q);
+      setMeals(querySnapshot.docs.map((doc) => doc.data()));
+    } catch (error) {
+      alert("Fehler beim Speichern: " + error.message);
+    }
+  };
 
   return (
-    <div>
-      <MealForm setMeals={setMeals} />
-      {/* Hier kannst du die meals-Liste anzeigen */}
-    </div>
+    <form onSubmit={handleAdd}>
+      {/* Eingabefelder */}
+    </form>
   );
 }
 
-export default NutritionPage;
+export default MealForm;
